a11f2b9d4066e660156603003806726373a8e9d6
Alexander Barnard <alexwb1@live.com>
2020-04-23 11:02:33 -0500

    Attempting to rebase again

--- aspnet/RVTR.Account.ObjectModel/Models/AccountModel.cs
+++ aspnet/RVTR.Account.ObjectModel/Models/AccountModel.cs
@@ -1,17 +1,5 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 
-namespace RVTR.Account.ObjectModel.Models
-{
-  public class AccountModel : IValidatableObject
-  {
-    public IEnumerable<ValidationResult> Validate(ValidationContext validationContext) => throw new System.NotImplementedException();
-=======
-=======
-using System.ComponentModel.DataAnnotations;
->>>>>>> Added validation annotations and hash fields.
 using System.Security.Cryptography;
 using RVTR.Account.ObjectModel.Util;
 
@@ -25,6 +13,6 @@ namespace RVTR.Account.ObjectModel.Models
     } } 
     public Profile[] Profile { get; set; } // Multiple profiles can be associated with one account, such as wife and kids all on one bill
     public AccountDetails AccountDetails { get; set; }
->>>>>>> 172281055 Added object models and Hash Util. Need to fix build errors and change some instance types to hash
+
   }
 }
