9e200e7af9d8b80796708ec18382517b31da30ba
Joshwin Greene <joshwin.greene@gmail.com>
2021-02-17 16:11:33 -0800

    Reduced the seeded data to just have one of each model

--- aspnet/RVTR.Account.Context/AccountContext.cs
+++ aspnet/RVTR.Account.Context/AccountContext.cs
@@ -26,84 +26,26 @@ namespace RVTR.Account.Context
       (
         new AccountModel
         {
-          EntityId = -1,
-          FirstName = "David",
-          LastName = "Dowd",
-          Email = "ddowd97@gmail.com"
-        },
-        new AccountModel()
-        {
           EntityId = 1,
-          FirstName = "David",
-          LastName = "Dowd",
-          Email = "jonsledge39@gmail.com"
-        },
-        new AccountModel()
-        {
-          EntityId = 2,
-          FirstName = "David",
-          LastName = "Dowd",
-          Email = "richard.noel@revature.net"
-        },
-        new AccountModel()
-        {
-          EntityId = 3,
-          FirstName = "David",
-          LastName = "Dowd",
-          Email = "sunzh95@gmail.com"
+          FirstName = "John",
+          LastName = "Smith",
+          Email = "j@gmail.com"
         }
       );
       modelBuilder.Entity<PaymentModel>().HasData
       (
         new PaymentModel()
         {
-          EntityId = -1,
+          EntityId = 1,
           CardExpirationDate = new DateTime(),
           CardNumber = "1234123412341234",
           CardName = "Visa",
           SecurityCode = "123",
-          AccountModelId = -1
-        },
-        new PaymentModel()
-        {
-          EntityId = 1,
-          AccountModelId = 1,
-          CardExpirationDate = new System.DateTime(2020, 08, 31),
-          CardNumber = "4111111111111111",
-          SecurityCode = "123",
-          CardName = "User's credit card"
-        },
-        new PaymentModel()
-        {
-          EntityId = 2,
-          AccountModelId = 2,
-          CardExpirationDate = new System.DateTime(9999, 01, 01),
-          CardNumber = "9999999999999999",
-          SecurityCode = "999",
-          CardName = "Richard's Trusty Card"
-        },
-        new PaymentModel()
-        {
-          EntityId = 3,
-          AccountModelId = 3,
-          CardExpirationDate = new System.DateTime(2020, 12, 01),
-          CardNumber = "1234567887654321",
-          SecurityCode = "010",
-          CardName = "Sun's Credit Card"
+          AccountModelId = 1
         }
       );
       modelBuilder.Entity<AddressModel>().HasData
       (
-        new AddressModel()
-        {
-          EntityId = -1,
-          City = "City",
-          Country = "USA",
-          PostalCode = "21345",
-          StateProvince = "NC",
-          Street = "123 elm street",
-          AccountId = -1,
-        },
         new AddressModel()
         {
           EntityId = 1,
@@ -112,74 +54,21 @@ namespace RVTR.Account.Context
           Country = "USA",
           PostalCode = "73301",
           StateProvince = "TX",
-          Street = "Test St"
-        },
-        new AddressModel()
-        {
-          EntityId = 2,
-          AccountId = 2,
-          City = "Seattle",
-          Country = "USA",
-          PostalCode = "65780",
-          StateProvince = "WA",
-          Street = "See Sharp St"
-        },
-        new AddressModel()
-        {
-          EntityId = 3,
-          AccountId = 3,
-          City = "West Lafayette",
-          Country = "USA",
-          PostalCode = "47906",
-          StateProvince = "IN",
-          Street = "272 Littleton St"
+          Street = "101 Blume Avenue"
         }
       );
       modelBuilder.Entity<ProfileModel>().HasData
       (
         new ProfileModel()
         {
-          EntityId = -1,
-          Email = "Test@test.com",
-          FamilyName = "Dowd",
-          GivenName = "David",
+          EntityId = 1,
+          Email = "j@gmail.com",
+          FamilyName = "Smith",
+          GivenName = "John",
           Phone = "1234567891",
           Type = "Adult",
-          AccountModelId = -1,
-          ImageUri = "https://avataaars.io/?avatarStyle=Circle&topType=LongHairStraight&accessoriesType=Blank&hairColor=Platinum&facialHairType=BeardLight&facialHairColor=Platinum&clotheType=CollarSweater&clotheColor=PastelYellow&eyeType=Default&eyebrowType=Angry&mouthType=Twinkle&skinColor=Brown"
-        },
-        new ProfileModel()
-        {
-          EntityId = 1,
           AccountModelId = 1,
-          Email = "demo.camper@revature.com",
-          FamilyName = "Sledge",
-          GivenName = "Jon",
-          Phone = "123-456-7891",
-          Type = "Child",
-          ImageUri = "https://avataaars.io/?avatarStyle=Circle&topType=LongHairFrida&accessoriesType=Round&hatColor=Blue01&facialHairType=MoustacheMagnum&facialHairColor=Black&clotheType=BlazerSweater&clotheColor=Heather&eyeType=Side&eyebrowType=SadConcerned&mouthType=Serious&skinColor=Brown"
-        },
-        new ProfileModel()
-        {
-          EntityId = 2,
-          AccountModelId = 2,
-          Email = "random@email.com",
-          FamilyName = "Noel",
-          GivenName = "Richard",
-          Phone = "123-456-7891",
-          Type = "Adult",
-          ImageUri = "https://avataaars.io/?avatarStyle=Circle&topType=LongHairCurly&accessoriesType=Prescription01&hairColor=Black&facialHairType=BeardMedium&facialHairColor=Auburn&clotheType=CollarSweater&clotheColor=White&eyeType=Close&eyebrowType=DefaultNatural&mouthType=Twinkle&skinColor=DarkBrown"
-        },
-        new ProfileModel()
-        {
-          EntityId = 3,
-          AccountModelId = 3,
-          Email = "anotherone@email.com",
-          FamilyName = "Sun",
-          GivenName = "Mr.",
-          Phone = "123-456-7891",
-          Type = "Adult",
-          ImageUri = "https://avataaars.io/?avatarStyle=Circle&topType=ShortHairDreads01&accessoriesType=Prescription01&hairColor=BlondeGolden&facialHairType=MoustacheFancy&facialHairColor=Auburn&clotheType=BlazerSweater&clotheColor=Gray02&eyeType=Cry&eyebrowType=UnibrowNatural&mouthType=Smile&skinColor=Brown"
+          ImageUri = "https://avataaars.io/?avatarStyle=Circle&topType=LongHairStraight&accessoriesType=Blank&hairColor=Platinum&facialHairType=BeardLight&facialHairColor=Platinum&clotheType=CollarSweater&clotheColor=PastelYellow&eyeType=Default&eyebrowType=Angry&mouthType=Twinkle&skinColor=Brown"
         }
       );
     }
