fd4f91f0ce271c9971bca29028bd6ae35591771d
Phuc Bao Pham <59951786+phambao2006@users.noreply.github.com>
2021-06-22 11:46:18 -0500

    Merge Profile controller to account controller Issue #67 (#84)
    - move profile controller to account controller
    
    Co-authored-by: chris young <christopher.young@revature.net>
    Co-authored-by: marcangy cange <marcangy.cange@revature.net>
    Co-authored-by: Stanhope Nwosu <stanhope.nwosu@gmail.com>

--- aspnet/RVTR.Account.Testing/Tests/ProfileModelTest.cs
+++ aspnet/RVTR.Account.Testing/Tests/ProfileModelTest.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using RVTR.Account.Domain.Models;
@@ -5,8 +6,10 @@ using Xunit;
 
 namespace RVTR.Account.Testing.Tests
 {
+
   public class ProfileModelTest
   {
+    private static DateTime adultAge = new DateTime(2003, 1, 30);
     public static readonly IEnumerable<object[]> Profiles = new List<object[]>
     {
       new object[]
@@ -18,10 +21,11 @@ namespace RVTR.Account.Testing.Tests
           FamilyName = "Family",
           GivenName = "Given",
           Phone = "1234567890",
+          DateOfBirth = adultAge,
           Type = "Adult",
           AccountModelId = 0
         }
-      }
+}
     };
 
     [Theory]
@@ -73,12 +77,26 @@ namespace RVTR.Account.Testing.Tests
     [Fact]
     public void Test_Profile_BadName()
     {
-      AccountModel account = new AccountModel("jim", " ","abc@gmail.com"); //bad name given (empty string for last name)
+      AccountModel account = new AccountModel("jim", " ", "abc@gmail.com", adultAge); //bad name given (empty string for last name)
 
       var validationContext = new ValidationContext(account.Profiles[0]);
       var actual = Validator.TryValidateObject(account.Profiles[0], validationContext, null, true);
 
       Assert.False(actual);
+    }
+
+    [Fact]
+    public void Test_Profile_Minor()
+    {
+      ProfileModel profile = new ProfileModel("", "", "", true, DateTime.Today);
+      Assert.False(profile.IsAdult);
+    }
+
+    [Fact]
+    public void Test_Profile_Adult()
+    {
+      ProfileModel profile = new ProfileModel("", "", "", true, adultAge);
+      Assert.True(profile.IsAdult);
     }
   }
 }
